ef8dee4b9daeea135c44840e0dd85cfb71e1542e
TalkWithKeyboard <610347922@qq.com>
2022-03-16 18:18:15 +0800

    feat: add progress logger to export_prices.

--- dags/ethereumetl_airflow/prices_provider/prices_provider.py
+++ dags/ethereumetl_airflow/prices_provider/prices_provider.py
@@ -2,6 +2,8 @@ import datetime
 import json
 from typing import List
 
+from ethereumetl.progress_logger import ProgressLogger
+
 
 class Pair:
     def __init__(self, pair: str, market: str):
@@ -21,6 +23,7 @@ class PricesProvider:
     def __init__(self, host: str, api_key: str):
         self.host = host
         self.api_key = api_key
+        self.progress_logger = ProgressLogger()
 
     def get_all_usd_pair(self) -> List[Pair]:
         raise NotImplementedError()
@@ -31,9 +34,12 @@ class PricesProvider:
     def create_temp_json(self, output_path: str, periods: int, start: int, end: int) -> str:
         with open(output_path, 'w') as f:
             pairs = self.get_all_usd_pair()
+            self.progress_logger.start(total_items=len(pairs))
 
-            for pair in pairs:
+            for index, pair in enumerate(pairs):
                 records = self.get_single_pair_daily_price(pair, periods, start, end)
                 f.write(json.dumps([record.__dict__ for record in records]))
+                self.progress_logger.track()
 
+        self.progress_logger.finish()
         return output_path
